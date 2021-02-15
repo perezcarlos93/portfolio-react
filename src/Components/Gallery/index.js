5595c654f4cf0a1478ff1182431ce68b7da8e856
Carlos <perez.carlos_a@yahoo.com>
2021-02-15 14:37:34 -0800

    refactored Gallery component css to work properly

--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -9,23 +9,37 @@ import css from '../../public/Assets/tech/css-logo.png';
 import github from '../../public/Assets/tech/github-logo.png';
 import react from '../../public/Assets/tech/react-logo.png';
 import handlebars from '../../public/Assets/tech/handlebars-logo.png';
+import headshot from '../../public/Assets/headshot.png';
 import './style.css';
 
 const Gallery = () => {
 	return (
-		<div class="techGallery row">
-			<img src={javascript} alt="js" class="techIcon" id="js"></img>
-			<img src={node} class="techIcon" id="node"></img>
-			<img src={express} class="techIcon" id="express"></img>
-			<img src={mongodb} class="techIcon" id="mongo"></img>
-			<img src={mysql} class="techIcon" id="mysql"></img>
-			<img src={html} class="techIcon" id="html"></img>
-			<img src={css} class="techIcon" id="css"></img>
-			<img src={github} class="techIcon" id="github"></img>
-			<img src={react} class="techIcon" id="react"></img>
-			<img src={handlebars} class="techIcon" id="handlebars"></img>
-
-			<img id="headshot" src="/Assets/headshot.png"></img>
+		<div className="row">
+			<div className="techGallery col-6">
+				<img src={javascript} alt="js" className="techIcon" id="js"></img>
+				<img src={node} alt="node" className="techIcon" id="node"></img>
+				<img
+					src={express}
+					alt="express"
+					className="techIcon"
+					id="express"
+				></img>
+				<img src={mongodb} alt="mongodb" className="techIcon" id="mongo"></img>
+				<img src={mysql} alt="mysql" className="techIcon" id="mysql"></img>
+				<img src={html} alt="html" className="techIcon" id="html"></img>
+				<img src={css} alt="css" className="techIcon" id="css"></img>
+				<img src={github} alt="github" className="techIcon" id="github"></img>
+				<img src={react} alt="react" className="techIcon" id="react"></img>
+				<img
+					src={handlebars}
+					alt="handlebars"
+					className="techIcon"
+					id="handlebars"
+				></img>
+			</div>
+			<div className="col-6">
+				<img id="headshot" src={headshot} alt="headshot"></img>
+			</div>
 		</div>
 	);
 };
