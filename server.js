59bd88177beeaa286eb8f3c9c042f0596c95f97d
Carlos <perez.carlos_a@yahoo.com>
2021-03-06 09:19:07 -0800

    removing app.get

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const path = require('path');
+// const path = require('path');
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
